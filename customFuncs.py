9aae4f77b72ace6760662289dd74afb5610ca159
Aditya Rajagopal <adityarajagopal@github.com>
2018-10-15 23:08:18 +0100

    backprop multiplies don't work

---
+++ customFuncs.py
@@ -0,0 +1,123 @@
+import math 
+import sys
+import cupy as cp 
+import torch
+from torch.autograd import Function
+from torch.autograd import Variable
+from torch.nn.modules.module import Module 
+from torch.nn.parameter import Parameter 
+from torch.utils.dlpack import to_dlpack, from_dlpack
+
+def correlate(ip, weight, padding, stride, corner_case=False, op_channels=None) : 
+    # unfold tensors
+    kernel_size = weight.size(2)
+    if corner_case : 
+        print('here')
+        o_channels = op_channels
+        ip_unfold = torch.nn.functional.unfold(ip, (kernel_size, kernel_size), padding=padding, stride=stride)
+        w_unfold = weight.unsqueeze(1).expand(-1, o_channels, -1, -1, -1)
+        w_unfold = w_unfold.view(w_unfold.size(0), w_unfold.size(1), -1)
+    else :
+        o_channels = weight.size(0)
+        ip_unfold = torch.nn.functional.unfold(ip, (kernel_size, kernel_size), padding=padding, stride=stride)
+        w_unfold = weight.view(o_channels, -1) 
+    
+    # print("kernel_size :", kernel_size)
+    # print("o_channels :", o_channels)
+    # print("input :", ip.size())
+    # print("input_unfold :", ip_unfold.size())
+    # print("wegiht :", weight.size())
+    # print("weight_unfold :", w_unfold.size())
+
+    # cupy conversion 
+    ip_cupy = cp.fromDlpack(to_dlpack(ip_unfold)).astype('int8')
+    w_cupy = cp.fromDlpack(to_dlpack(w_unfold)).astype('int8')
+
+    # print(ip_cupy.shape)
+    # print(w_cupy.shape)
+
+    # multiplication in cupy 
+    if corner_case : 
+        op = cp.matmul(cp.transpose(ip_cupy, (0,2,1)), cp.transpose(w_cupy, (0,2,1)))
+        op = cp.transpose(op, (0,2,1))
+        print(op.shape)
+        sys.exit()
+    else : 
+        op = cp.matmul(cp.transpose(ip_cupy, (0,2,1)), cp.transpose(w_cupy))
+        op = cp.transpose(op, (0,2,1))
+        print(op.shape)
+
+
+    return op 
+
+class Conv2dFunc(Function) : 
+    @staticmethod
+    def forward(context, ip, weight, bias, stride, padding, dilation, kernel) : 
+        op = correlate(ip, weight, padding, stride)        
+        
+        bias_repeat = bias.repeat(op.shape[0], op.shape[2]).view(op.shape[0], op.shape[2], bias.shape[0])
+        bias_cupy = cp.fromDlpack(to_dlpack(bias_repeat)).astype('int8')
+        op = cp.add(cp.transpose(op, (0,2,1)), bias_cupy) 
+        op = cp.transpose(op, (0,2,1))
+        
+        # conversion back to pytorch 
+        op_dlpack = op.toDlpack()
+        op_tensor = from_dlpack(op_dlpack).float()
+
+        # fold op tensor 
+        h_in = ip.size()[2]
+        w_in = ip.size()[3]
+        o_dim_x = math.floor((((h_in + 2*padding) - (dilation * (kernel - 1)) - 1)/(stride))+1)
+        o_dim_y = math.floor((((w_in + 2*padding) - (dilation * (kernel - 1)) - 1)/(stride))+1)
+
+        op_dim = (o_dim_x, o_dim_y)
+        op_fold = torch.nn.functional.fold(op_tensor, op_dim, (1,1))
+
+        context.save_for_backward(ip, weight, bias, Variable(torch.Tensor([padding]).type(torch.cuda.IntTensor), requires_grad=False), Variable(torch.tensor([stride]).type(torch.cuda.IntTensor), requires_grad=False), Variable(torch.Tensor([kernel]).type(torch.cuda.IntTensor)))
+        
+        return torch.as_tensor(op_fold)
+
+    @staticmethod 
+    def backward(context, grad_wrt_op) : 
+        # retreive saved tensors 
+        ip, weight, bias, padding, stride, kernel = context.saved_tensors 
+        padding = int(padding.data.tolist()[0])
+        stride = int(stride.data.tolist()[0])
+        kernel = int(kernel.data.tolist()[0])
+
+        # grad wrt weight matrix 
+        grad_wrt_weight = correlate(ip, grad_wrt_op, padding, stride, True, weight.size(0))
+        grad_wrt_weight_tensor = from_dlpack(grad_wrt_weight.toDlpack()).float()
+        grad_wrt_weight_tensor = torch.nn.functional.fold(grad_wrt_weight_tensor, (weight.size()[2], weight.size()[3]), (1,1))
+
+        # grad wrt input matrix 
+        grad_wrt_input = correlate(grad_wrt_op, torch.flip(torch.flip(weight, [0]), [1]), padding, stride) 
+        grad_wrt_input_tensor = from_dlpack(grad_wrt_input.toDlpack()).float()
+        grad_wrt_input_tensor = torch.nn.functional.fold(grad_wrt_input_tensor, (ip.size()[2], ip.size()[3]), (1,1))
+
+        # grad wrt bias 
+        grad_wrt_bias_tensor = torch.sum(grad_wrt_op, (0,1,2), keepdim=True)
+        # grad_wrt_bias_tensor = from_dlpack(grad_wrt_bias.toDlpack()).float()
+
+        return torch.as_tensor(grad_wrt_input_tensor), torch.as_tensor(grad_wrt_weight_tensor), torch.as_tensor(grad_wrt_bias_tensor), None, None, None, None
+
+class Conv2d(Module) : 
+    def __init__(self, i_channels, o_channels, kernel_size, stride=1, padding=0) :
+        super(Conv2d, self).__init__()
+        
+        self.i_channels = i_channels 
+        self.o_channels = o_channels 
+        self.kernel_size = kernel_size 
+        self.stride = stride 
+        self.padding = padding 
+        self.dilation = 1
+
+        # self.weight = Parameter(torch.randint(-128, 127, (o_channels, i_channels, kernel_size, kernel_size), dtype=torch.int8))
+        # self.bias = Parameter(torch.randint(-128, 127, (o_channels), dtype=torch.int8))
+        self.weight = Parameter(torch.randn((o_channels, i_channels, kernel_size, kernel_size)))
+        self.bias = Parameter(torch.randn((o_channels)))
+
+    def forward(self, ip) : 
+        return Conv2dFunc.apply(ip, self.weight, self.bias, *(self.stride, self.padding, 1, self.kernel_size))
+
+
--- models/cifar/alexnet.py
+++ models/cifar/alexnet.py
@@ -6,8 +6,11 @@ import torch
 import torch.nn as nn
 import sys
 import time
+import math 
 # from quantiser import quantise_inputs
 from .quantisationblock import QuantisationBlock
+from pyinn.modules import Conv2dDepthwise 
+from customFuncs import Conv2d 
 
 
 __all__ = ['alexnet']
@@ -17,11 +20,16 @@ class AlexNet(nn.Module):
 
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5)
-        self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
-        self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5)
+        # self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
+        # self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
+        # self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+        # self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv1 = Conv2d(3, 64, kernel_size=11, stride=4, padding=5)
+        self.conv2 = Conv2d(64, 192, kernel_size=5, padding=2)
+        self.conv3 = Conv2d(192, 384, kernel_size=3, padding=1)
+        self.conv4 = Conv2d(384, 256, kernel_size=3, padding=1)
+        self.conv5 = Conv2d(256, 256, kernel_size=3, padding=1)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool2d = nn.MaxPool2d(kernel_size=2, stride=2)
         
@@ -30,11 +38,45 @@ class AlexNet(nn.Module):
     def forward(self, x):
         times = torch.FloatTensor([])
         
+        # print("input size : ", x.size())
+        # print("weight size : ", self.conv1.weight.size())
+        # print("bias size : ", self.conv1.bias.size())
         x = self.conv1(x)
         
         x = self.relu(x)
         
         x = self.maxpool2d(x)
+        
+        # print("input size : ", x.size())
+        # print("weight size : ", self.conv2.weight.size())
+        # print("bias size : ", self.conv2.bias.size())
+        # tmp1 = torch.nn.functional.unfold(x, (5,5), padding=2)
+        # tmp2 = self.conv2.weight.view(self.conv2.weight.size(0), -1)
+        # print("input unfolded : ", tmp1.size()) 
+        # print("weight unfolded : ", tmp2.size())
+        # out1 = tmp1.transpose(1,2).matmul(tmp2.t())
+        # print("output : ", out1.size()) 
+
+        # tmp_bias = self.conv2.bias.repeat(out1.size()[0], out1.size()[1]).view(out1.size()[0], out1.size()[1], self.conv2.bias.size()[0])
+        # print("bias repeated :", tmp_bias.size())
+        # out1 += tmp_bias
+        # out1 = out1.transpose(1,2)
+        # print(out1.size())
+        # 
+        # h_in = x.size()[2]
+        # w_in = x.size()[3] 
+        # padding = (2,2)
+        # dilation = (1,1) 
+        # kernel = (5,5) 
+        # stride = (1,1) 
+        # o_dim_x = math.floor((((h_in + 2*padding[0]) - (dilation[0] * (kernel[0] - 1)) - 1)/(stride[0]))+1)
+        # o_dim_y = math.floor((((w_in + 2*padding[1]) - (dilation[1] * (kernel[1] - 1)) - 1)/(stride[1]))+1)
+
+        # out2 = torch.nn.functional.fold(out1, (o_dim_x, o_dim_y), (1,1))
+        # print("folded output : ", out2.size())
+
+
+        # sys.exit()
         
         x = self.conv2(x) 
         
